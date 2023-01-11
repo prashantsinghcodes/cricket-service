51ebb20654f578b538f8ace7d0820a1cd8d7e1c5
Prashant Singh <prashantsingh4787@gmail.com>
2023-01-12 00:28:40 +0530

    Migrating app from maven to spring boot

---
+++ src/main/java/com/ps/cricketservice/CricketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ps.cricketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/cricketservice/CricketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.cricketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
