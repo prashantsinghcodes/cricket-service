0e4d1fee2700afe382ea13c2fbc20116d73dbed6
Prashant Singh <prashantsingh4787@gmail.com>
2023-01-12 13:30:28 +0530

    adding thymeleaf as ui

--- src/main/java/com/ps/cricketservice/controller/CricketController.java
+++ src/main/java/com/ps/cricketservice/controller/CricketController.java
@@ -1,10 +1,13 @@
 package com.ps.cricketservice.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,9 +23,9 @@ public class CricketController {
 	private CricketService cricketService;
 	
 	@GetMapping(path = "/summary")
-	public String getSummary(ModelMap modelMap) {
+	public String getSummary(Model modelMap) {
 		logger.info("received request from ui...");
-		modelMap.addAttribute("summaryList", cricketService.retriveSummary());
+        modelMap.addAttribute("summaryList", cricketService.retriveSummary());
 		return "summary";
 	}
 }
